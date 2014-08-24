35ff358aa3f1e67a587f6c1efb190909a6558c67
Tapio Vierros <tapiovierros@gmail.com>
2014-08-24 19:56:33 +0300

    Click to jump.

--- js/controller.js
+++ js/controller.js
@@ -3,24 +3,30 @@ function Controller(obj) {
 	this.object = obj;
 	var pressed = [];
 
-	function onKeyDown(event) {
-		pressed[event.keyCode] = true;
+	function onKeyDown(e) {
+		pressed[e.keyCode] = true;
 		if (pressed[17] || pressed[18]) // CTRL/ALT for browser hotkeys
 			return;
-		if (event.keyCode >= 112 && event.keyCode <= 123) // F1-F12
+		if (e.keyCode >= 112 && e.keyCode <= 123) // F1-F12
 			return;
 
-		if (event.keyCode == 32)
+		if (e.keyCode == 32) // Space
 			obj.jump();
 
-		event.preventDefault();
+		e.preventDefault();
 	}
 
-	function onKeyUp(event) {
-		pressed[event.keyCode] = false;
-		event.preventDefault();
+	function onKeyUp(e) {
+		pressed[e.keyCode] = false;
+		e.preventDefault();
+	}
+
+	function onClick(e) {
+		obj.jump();
+		e.preventDefault();
 	}
 
 	document.addEventListener('keydown', onKeyDown, true);
 	document.addEventListener('keyup', onKeyUp, true);
+	document.addEventListener('click', onClick, true);
 };

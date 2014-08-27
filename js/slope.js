d88ec7564c5a851f9ca904737644820d63dbc63f
Antti Virta <antti.virta@gmail.com>
2014-08-27 17:04:58 +0300

    Slope.js refactoring + added field startingPosition

--- js/slope.js
+++ js/slope.js
@@ -55,28 +55,28 @@ function FISSlope(world, scene) {
 	var degToRad = 0.0174532925;
 	var HS = 150; // Hill size
 	var w = 0.885*HS+1.5;
-	var gamma = 32; // Inclination of the inrun slope
-	var alpha = 5; // Take-off table inclination
+	var gamma = 32 * degToRad; // Inclination of the inrun slope
+	var alpha = 5 * degToRad; // Take-off table inclination
 	var t = 10; // Length of the take-off table
 	var r1 = 10; // Curve radius for take-off slope transition end point
 	
 	var e1 = 92.3 - 1.517*gamma + 0.426*HS // Highest starting point
 	var e2 = 67.3 - 0.944*gamma + 0.331*HS // Lowest starting point
 	
-	var sin_ga = Math.sin(degToRad*(gamma-alpha));
-	var cos_ga = Math.cos(degToRad*(gamma-alpha));
-	var tan_ga = Math.tan(degToRad*(gamma-alpha));
+	var sin_ga = Math.sin(gamma-alpha);
+	var cos_ga = Math.cos(gamma-alpha);
+	var tan_ga = Math.tan(gamma-alpha);
 	
 	var d = 2*r1*sin_ga*cos_ga*cos_ga;
 	var C = tan_ga/3/(d*d);
 	var f = tan_ga*d/3;
 	var l = d*(1+0.1*tan_ga*tan_ga);
 	
-	var E1x = -(t*Math.cos(degToRad*alpha)+f*Math.sin(degToRad*gamma)+d*Math.cos(degToRad*gamma));
-	var E1y = t*Math.sin(degToRad*alpha)-f*Math.cos(degToRad*gamma)+d*Math.sin(degToRad*gamma);
+	var E1x = -(t*Math.cos(alpha)+f*Math.sin(gamma)+d*Math.cos(gamma));
+	var E1y = t*Math.sin(alpha)-f*Math.cos(gamma)+d*Math.sin(gamma);
 	
-	var E2x = -t*Math.cos(degToRad*alpha);
-	var E2y = t*Math.sin(degToRad*alpha);
+	var E2x = -t*Math.cos(alpha);
+	var E2y = t*Math.sin(alpha);
 	
 	var slopeProfile = [];
 	var slope = new p2.Body();
@@ -94,17 +94,17 @@ function FISSlope(world, scene) {
 
 	for (var i = 0; i < nIter - 1; ++i) 
 	{
-		var P = 1/Math.tan(degToRad*gamma)/3/C;
-		var Q = (xCur + t*Math.cos(degToRad*alpha)+f*Math.sin(degToRad*gamma)+d*Math.cos(degToRad*gamma))/2/C/Math.sin(degToRad*gamma);
+		var P = 1/Math.tan(gamma)/3/C;
+		var Q = (xCur + t*Math.cos(alpha)+f*Math.sin(gamma)+d*Math.cos(gamma))/2/C/Math.sin(gamma);
 		
 		var ksi = Math.pow((Math.pow((Q*Q+P*P*P),0.5)+Q),0.3333) - Math.pow((Math.pow((Q*Q+P*P*P),0.5)-Q),0.3333);
-		var yTran = t*Math.sin(degToRad*alpha)-f*Math.cos(degToRad*gamma)+d*Math.sin(degToRad*gamma)-ksi*Math.sin(degToRad*gamma)+C*ksi*ksi*ksi*Math.cos(degToRad*gamma);
+		var yTran = t*Math.sin(alpha)-f*Math.cos(gamma)+d*Math.sin(gamma)-ksi*Math.sin(gamma)+C*ksi*ksi*ksi*Math.cos(gamma);
 		slopeProfile.push([xCur, yTran]);
 		xCur -= xIncr;
 	}
 	slopeProfile.push([E1x, E1y]); // Start of transition segment
-	slopeProfile.push([E1x-(e1-l)*Math.cos(degToRad*gamma),E1y+(e1-l)*Math.sin(degToRad*gamma)]); // Starting position
-	slopeProfile.push([E1x-(e1-l)*Math.cos(degToRad*gamma), -40]);
+	slopeProfile.push([E1x-(e1-l)*Math.cos(gamma),E1y+(e1-l)*Math.sin(gamma)]); // Starting position
+	slopeProfile.push([E1x-(e1-l)*Math.cos(gamma), -40]);
 	slopeProfile.push([120, -40]);
 	slope.fromPolygon(slopeProfile);
 	
@@ -113,6 +113,9 @@ function FISSlope(world, scene) {
 		slope.shapes[i].material = new p2.Material();
 	}
 	this.body = slope;
+	
+	// Calculate starting point for jumper
+	this.startingPosition = [E1x-(e1-l-1)*Math.cos(gamma), E1y+(e1-l)*Math.sin(gamma)+1];
 	world.addBody(this.body);
 
 	// Visual representation

7d22378281aa6ed9436e7245b7c88b250de37346
Antti Virta <antti.virta@gmail.com>
2014-09-01 20:47:08 +0300

    Jumper rotation (no body)

--- js/jumper.js
+++ js/jumper.js
@@ -10,7 +10,7 @@ var JumperState = {
 
 function Jumper(world, scene) {
 	// Physical body
-	var jumperHeight = 1.7;
+	var jumperHeight = 1.85;
 	var skiLength = 2.7;
 	this.skiLength = skiLength;
 	this.skisShape = new p2.Rectangle(skiLength, 0.02);
@@ -20,19 +20,22 @@ function Jumper(world, scene) {
 
 	this.body = new p2.Body({ mass: 55 });
 	this.body.addShape(this.skisShape);
-	this.body.addShape(this.jumperShape, [0, jumperHeight * 0.5]);
+	//this.body.addShape(this.jumperShape, [0, jumperHeight * 0.5]);
 	world.addBody(this.body);
 
 	// Visual representation
 	var skiGeometry = new THREE.PlaneGeometry(this.skisShape.width, 0.2);
 	var jumperGeometry = new THREE.PlaneGeometry(this.jumperShape.width, this.jumperShape.height);
-	var material = new THREE.MeshBasicMaterial({ color: 0x2222ff });
+	var skiMaterial = new THREE.MeshBasicMaterial({ color: 0x2222ff });
 	this.visual = new THREE.Object3D();
-	var skiMesh = new THREE.Mesh(skiGeometry, material);
+	var skiMesh = new THREE.Mesh(skiGeometry, skiMaterial);
 	skiMesh.position.y = 0.1;
 	this.visual.add(skiMesh);
-	var jumperMesh = new THREE.Mesh(jumperGeometry, material);
-	jumperMesh.position.y = this.jumperShape.height * 0.5;
+	var jumperMaterial = new THREE.MeshBasicMaterial({ color: 0x22ff22 });
+	var jumperMesh = new THREE.Mesh(jumperGeometry, jumperMaterial);
+	jumperMesh.position.x = -1/5*this.skiLength;
+	jumperMesh.position.y = this.jumperShape.height * 0.5 + 0.1;
+	jumperMesh.rotation.z = -10 * Math.PI / 180;
 	this.visual.add(jumperMesh);
 	scene.add(this.visual);
 
@@ -46,9 +49,10 @@ Jumper.prototype.reset = function() {
 	this.topSpeed = 0;
 	this.body.sleep();
 	var slopeStartingPos = ramp.startingPosition;
-	this.body.position[0] = slopeStartingPos[0]; // TODO: Get from slope?
+	this.body.position[0] = slopeStartingPos[0];
 	this.body.position[1] = slopeStartingPos[1];
 	this.body.angle = 0;
+	this.jumperAngle = 0;
 	var msg = isTouchDevice ? "Tap to start" : "Click to start";
 	$("#hint").innerHTML = msg;
 	$("#results").style.display = "none";
@@ -66,7 +70,7 @@ Jumper.prototype.action = function() {
 		case JumperState.SLIDING:
 			if (this.isOnRamp() && this.body.position[0] > -20) { // TODO: Right amount of x
 				this.body.velocity[1] = 10;
-				this.body.angle = 10.5*Math.PI/180;	
+				this.body.angle = 0;	
 				this.state = JumperState.FLYING; // TODO: Should go to Jumping state to charge the jump
 				this.stateTime = 0;
 			}
@@ -94,7 +98,7 @@ Jumper.prototype.steer = function(steer) {
 			break;
 		case JumperState.FLYING:
 			// TODO: This is just a place holder, should rotate jumper and not skis
-			this.body.angle -= 1 * steer; // Radians per second
+			this.jumperAngle -= 1 * steer; // Radians per second
 			break;
 		case JumperState.LANDING:
 			break;
@@ -151,6 +155,9 @@ Jumper.prototype.update = function(dt) {
 	this.visual.position.x = this.body.position[0];
 	this.visual.position.y = this.body.position[1];
 	this.visual.rotation.z = this.body.angle;
+	this.visual.children[1].position.x = -1/5*this.skiLength - Math.sin(this.jumperAngle)*0.5*1.7;
+	this.visual.children[1].position.y = this.jumperShape.height * 0.5 + 0.1 - 0.5*this.jumperShape.height*(1-Math.cos(this.jumperAngle))
+	this.visual.children[1].rotation.z = this.jumperAngle;
 }
 
 Jumper.prototype.isOnRamp = function() {
@@ -179,7 +186,7 @@ Jumper.prototype.physics = function() {
 	// Various angles (in degrees)
 	var beta = 9.5 * Math.PI/180; // Body-to-ski
 	var gamma = 160 * Math.PI/180; // Hip angle
-	var alpha = this.body.angle; //35.5; // Angle of attack, this should be calculated based on jumper orientation and airspeed
+	var alpha = this.jumperAngle; //35.5; // Angle of attack, this should be calculated based on jumper orientation and airspeed
 	
 	// Lift and drag coefficients
 	var cD = 0.1;

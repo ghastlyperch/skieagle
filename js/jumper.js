e62140409c4db6c1baac9be9963e4ade9cb957d0
Tapio Vierros <tapiovierros@gmail.com>
2014-12-30 13:02:44 +0200

    Params to own file.

--- js/jumper.js
+++ js/jumper.js
@@ -13,21 +13,6 @@ var PhysicsMode = {
 	SIMPLE_LIFTDRAG: 1
 };
 
-var Params = {
-	
-	Jumper: {
-		height: 1.78,
-		mass: 65,
-		skiLength: 2.5,
-		takeoffStr: 0.05,
-		takeoffTargetAngle: -85 * Math.PI/180,
-		landingTargetAngle: -15 * Math.PI/180,
-		angVelToFlyPosition: -2.4,
-		angVelToLandingPosition: 2.4
-	}
-
-};
-
 function Jumper(world, scene) {
 	// Physics solver mode
 	this.physicsMode = PhysicsMode.PARAMETRIC;
---
+++ js/params.js
@@ -0,0 +1,14 @@
+var Params = {
+
+	Jumper: {
+		height: 1.78,
+		mass: 65,
+		skiLength: 2.5,
+		takeoffStr: 0.05,
+		takeoffTargetAngle: -85 * Math.PI/180,
+		landingTargetAngle: -15 * Math.PI/180,
+		angVelToFlyPosition: -2.4,
+		angVelToLandingPosition: 2.4
+	}
+
+};
